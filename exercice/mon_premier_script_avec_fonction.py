38dac66494118aafd7958b373d086fb9956b118e
AdrienHou <adrien.houssoy@gmail.com>
2026-02-16 15:35:04 +0000

    feat: Code refactoring mon_premier_script_avec_fonction.py

--- exercice/mon_premier_script_avec_fonction.py
+++ exercice/mon_premier_script_avec_fonction.py
@@ -1,23 +1,32 @@
 import unittest
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
-
+# -------------------------------------- Class ---------------------------------------------
 class TestNamesMethod(unittest.TestCase):
      def test_names(self):
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+        lst_names = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        nb_names = count_names_more_7_caracters(names=lst_names)
+        self.assertEqual(nb_names, 4) # Check if the number of name with more than 7 caracters are 4
 
+# -------------------------------------- Function ---------------------------------------------
+def count_names_more_7_caracters(names : list) -> int:
+    """
+    Count names with more than seven letters
+
+    Param: 
+        - names (list) : list with names to count
+    
+    return: 
+        - more_than_seven (int) : Number of name with more than 7 caracters
+    """
+    more_than_seven = 0
+    for name in names:
+        if len(name) > 7:
+            more_than_seven += 1
+            print(name + " est un prénom avec un nombre de lettres supérieur à 7")
+        else:
+            print(name + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
+    return more_than_seven
+
+# -------------------------------------- Launcher ---------------------------------------------
 if __name__=='__main__':
     unittest.main()
