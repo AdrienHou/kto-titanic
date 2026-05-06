52a7f3dd6e45c96ed4a75c2161f82ac60319ef41
AdrienHou <adrien.houssoy@gmail.com>
2026-05-06 19:03:04 +0000

    feat: Configuration Jaeger

--- src/titanic/api/infer.py
+++ src/titanic/api/infer.py
@@ -6,42 +6,49 @@ ou directement par un front. Remplir ce script une fois l'entrainement du model 
 
 import os
 import pickle
-
-# TODO : Importer les dépendances utiles au bon développement en Python (dataclass, enum, pandas)
 from dataclasses import dataclass
 from enum import Enum
 import pandas as pd
 
-# TODO : Importer les dépendances pour sérialiser / désérialiser le model
-
-from fastapi import FastAPI,Depends
-# TODO : Importer les dépendances OTEL pour le monitoring
+from fastapi import FastAPI, Depends
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter as HTTPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
 
 from titanic.api.auth import verify_token
 
 
 JAEGER_ENDPOINT = os.getenv("JAEGER_ENDPOINT", "http://jaeger.kto-gthomas-dev.svc.cluster.local:4318/v1/traces")
 
-# TODO : Intégrer les configurations d'OTEL et instancier le tracer. Peut être fait plus tard si le cours
-# sur l'observabilité n'est pas encore donné
+resource = Resource(attributes={"service.name": "titanic-inference-api"})
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(HTTPSpanExporter(endpoint=JAEGER_ENDPOINT))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer(__name__)
 
 app = FastAPI()
+FastAPIInstrumentor.instrument_app(app)
 
 with open("./src/titanic/api/resources/model.pkl", "rb") as f:
     model = pickle.load(f)
 
-# TODO : Créer les class et dataclass représentant la donnée qui sera transmise au Webservice pour l'inférence
-
-# TODO : Créer Pclass (enum)
 class Pclass(Enum):
     UPPER = 1
     MIDDLE = 2
     LOW = 3
-# TODO : Créer Sex (enum)
+
+
 class Sex(Enum):
     MALE = "male"
     FEMALE = "female"
-# TODO : Créer Passenger (attention, l'objet doit pouvoir être transmis en dictionnaire au model. Il faudra créer une méthode d'instance
+
+
 @dataclass
 class Passenger:
     pclass: Pclass
@@ -52,21 +59,25 @@ class Passenger:
     def to_dict(self) -> dict:
         return {"Pclass": self.pclass.value, "Sex": self.sex.value, "SibSp": self.sibSp, "Parch": self.parch}
 
-
 @app.get("/health")
 def health() -> dict:
     return {"status": "OK"}
 
-# TODO : Faire en sorte que cette fonction soit exposée via une route POST /infer
-# TODO : Ajouter les paramètres de la fonction (peut se faire en deux fois avec la sécurisation via oAuth2)
 @app.post("/infer")
 def infer(passenger: Passenger, token: str = Depends(verify_token("api:read"))) -> list:
+    with tracer.start_as_current_span("model_inference") as span:
+        span.set_attribute("passenger.pclass", passenger.pclass.value)
+        span.set_attribute("passenger.sex", passenger.sex.value)
+        span.set_attribute("passenger.sibsp", passenger.sibSp)
+        span.set_attribute("passenger.parch", passenger.parch)
 
-    df_passenger = pd.DataFrame([passenger.to_dict()])
-    df_passenger["Sex"] = pd.Categorical(df_passenger["Sex"], categories=[Sex.FEMALE.value, Sex.MALE.value])
-    df_to_predict = pd.get_dummies(df_passenger)
+        df_passenger = pd.DataFrame([passenger.to_dict()])
+        df_passenger["Sex"] = pd.Categorical(df_passenger["Sex"], categories=[Sex.FEMALE.value, Sex.MALE.value])
+        df_to_predict = pd.get_dummies(df_passenger)
 
-    res = model.predict(df_to_predict)
+        res = model.predict(df_to_predict)
 
-    return res.tolist()
+        span.set_attribute("prediction.result", int(res[0]))
+        span.add_event("prediction_completed", {"result": int(res[0])})
 
+        return res.tolist()
